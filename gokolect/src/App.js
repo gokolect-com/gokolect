d8c6cf4b74afa9f63aa08680841f29af4aa4c765
ChikenduHillary <chikenduhillarychiagoziemu@gmail.com>
2022-11-15 13:53:09 +0100

    refactored code base, removed boostrap template, created sign in page

--- gokolect/src/App.js
+++ gokolect/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+import SignIn from './components/sign-in/sign-in.component';
+
 function App() {
   return (
     <div className="App">
-     
+     <SignIn />
     </div>
   );
 }
